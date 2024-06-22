6c4da6a424b14f7f9a1e7c1cbdc414b39a19d9bb
sairampothem <sairampothem@gmail.com>
2024-06-22 10:59:32 +0530

    Array example program

---
+++ array.rb
@@ -0,0 +1,10 @@
+# creating string using []
+str = ["sai","ram","riya","harsha"]
+
+#accessing array elements
+#using index
+puts str[1]
+
+#using negative index
+# array negative index start from -1
+puts str[-1]
